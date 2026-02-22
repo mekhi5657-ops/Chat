66e48f65467f1f485a19983e32fa1d929dce8d4e
mekhi5657-ops <mekhi5657@gmail.com>
2026-02-22 13:50:02 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
 const http = require("http");
 const { Server } = require("socket.io");
 
@@ -6,50 +8,78 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let rooms = {};
+app.use(session({
+  secret: "supersecretkey",
+  resave: false,
+  saveUninitialized: false
+}));
 
+// Fake database (in memory)
+let users = [];
+
+// ===== REGISTER =====
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  users.push({ username, password: hashedPassword });
+
+  res.redirect("/login.html");
+});
+
+// ===== LOGIN =====
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find(u => u.username === username);
+
+  if (!user) return res.send("User not found");
+
+  const valid = await bcrypt.compare(password, user.password);
+
+  if (!valid) return res.send("Wrong password");
+
+  req.session.user = username;
+  res.redirect("/chat.html");
+});
+
+// ===== AUTH CHECK =====
+app.get("/checkAuth", (req, res) => {
+  if (req.session.user) {
+    res.json({ loggedIn: true, user: req.session.user });
+  } else {
+    res.json({ loggedIn: false });
+  }
+});
+
+// ===== LOGOUT =====
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/login.html");
+});
+
+// ===== SOCKET.IO =====
 io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
-
-  socket.on("joinRoom", ({ username, room }) => {
-    socket.join(room);
+  socket.on("joinRoom", (username) => {
     socket.username = username;
-    socket.room = room;
-
-    if (!rooms[room]) rooms[room] = [];
-    rooms[room].push(username);
-
-    io.to(room).emit("message", {
+    io.emit("message", {
       user: "System",
-      text: `${username} joined the room`,
+      text: `${username} joined the chat`
     });
-
-    io.to(room).emit("roomUsers", rooms[room]);
   });
 
   socket.on("chatMessage", (msg) => {
-    io.to(socket.room).emit("message", {
+    io.emit("message", {
       user: socket.username,
-      text: msg,
+      text: msg
     });
   });
-
-  socket.on("disconnect", () => {
-    if (socket.room && rooms[socket.room]) {
-      rooms[socket.room] = rooms[socket.room].filter(
-        (u) => u !== socket.username
-      );
-      io.to(socket.room).emit("message", {
-        user: "System",
-        text: `${socket.username} left the room`,
-      });
-      io.to(socket.room).emit("roomUsers", rooms[socket.room]);
-    }
-  });
 });
 
-server.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
-});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log("Server running"));
